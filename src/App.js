89b992c8e6eb62619c96c0ade572c2d22a85f537
Hunter <bobnamed123@yahoo.com>
2022-07-18 15:26:56 -0700

    second

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World
-        </p>
-        
-      </header>
+    <div className="bg-image min-h-screen">
+      <Navbar />
     </div>
+    
   );
 }
 
