5a84a70adc942b3dab9a942eccaea0734e671494
Hunter <bobnamed123@yahoo.com>
2022-08-03 14:32:19 -0700

    got plaid working in sandbox

---
+++ server/index.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const { Configuration, PlaidApi, PlaidEnvironments } = require("plaid");
+const app = express();
+
+const PORT = 4090;
+var PLAID_CLIENT_ID = "62db0b76f42efd00135a59ac";
+var PLAID_SECRET = "697ecaba740887bdfcb56c94b55c3a";
+var PLAID_ENV = 'sandbox'
+
+
+app.use(
+  // FOR DEMO PURPOSES ONLY
+  // Use an actual secret key in production
+  session({ secret: "bosco", saveUninitialized: true, resave: true })
+);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Configuration for the Plaid client
+const config = new Configuration({
+  basePath: PlaidEnvironments[PLAID_ENV],
+  baseOptions: {
+    headers: {
+      "PLAID-CLIENT-ID": PLAID_CLIENT_ID,
+      "PLAID-SECRET": PLAID_SECRET,
+      "Plaid-Version": "2020-09-14",
+    },
+  },
+});
+
+//Instantiate the Plaid client with the configuration
+const client = new PlaidApi(config);
+
+//Creates a Link token and return it
+app.get("/api/create_link_token", async (req, res, next) => {
+  const tokenResponse = await client.linkTokenCreate({
+    user: { client_user_id: req.sessionID },
+    client_name: "Plaid's Tiny Quickstart",
+    language: "en",
+    products: ["auth"],
+    country_codes: ["US"],
+  });
+  res.json(tokenResponse.data);
+});
+
+// Exchanges the public token from Plaid Link for an access token
+app.post("/api/exchange_public_token", async (req, res, next) => {
+  const exchangeResponse = await client.itemPublicTokenExchange({
+    public_token: req.body.public_token,
+  });
+
+  // FOR DEMO PURPOSES ONLY
+  // Store access_token in DB instead of session storage
+  req.session.access_token = exchangeResponse.data.access_token;
+  res.json(true);
+});
+
+// Fetches balance data using the Node client library for Plaid
+app.get("/api/balance", async (req, res, next) => {
+  const access_token = req.session.access_token;
+  const balanceResponse = await client.accountsBalanceGet({ access_token });
+  res.json({
+    Balance: balanceResponse.data,
+  });
+});
+
+console.log("on port: ", PORT)
+app.listen(PORT);
